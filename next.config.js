d9809b2422fb5a5afbcdda3e540f75ac78b160b9
Svetlana.Novikova <svetlana.novikova@jetbrains.com>
2026-03-14 17:43:33 +0100

    fix errors on the launch

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
   // Prevent exposing of environment variables not prefixed with NEXT_PUBLIC_
   // This is default behavior, explicitly specified for clarity
   serverExternalPackages: [],
-
-  // Webpack configuration for Web Workers
-  webpack: (config) => {
-    // Support for Web Workers
-    config.module.rules.push({
-      test: /\.worker\.(js|ts)$/,
-      use: { loader: 'worker-loader' },
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
